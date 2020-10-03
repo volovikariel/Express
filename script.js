e86e0ef7132f20c358a0bb77f1ea8953ce49a142
volovikariel <volovikariel@gmail.com>
2020-10-02 20:46:56 -0400

    Sends static files in the static subfolder

--- script.js
+++ script.js
@@ -2,52 +2,21 @@
 let path = require("path");
 let http = require("http");
 let express = require("express");
-let logger = require("morgan");
-// Allows for the parsing of Post requests
-let bodyParser = require("body-parser");
+let morgan = require("morgan");
 
 let app = express();
 
-// Set up views 
-app.set("views", path.resolve(__dirname, "views"));
-app.set("view engine", "ejs");
-
-// Make an array to store the guestbook entries
-let entries = [];
-app.locals.entries = entries;
-
 // Use logger 
-app.use(logger("dev"));
+app.use(morgan("short"));
 
-// Parse the body with bodyParser 
-app.use(bodyParser.urlencoded({extended: false}));
-
-// Render the different views on GET requests
-app.get("/", (req, res) => {
-    res.render("index");
-})
-
-app.get("/new-entry", (req, res) => {
-    res.render("new-entry");
-})
-
-// Push to entries when receiving a POST request
-app.post("/new-entry", (req, res) => {
-    if(!req.body.title || !req.body.body) {
-        res.status(400).end("Entried must have a title and a body!");
-        return;
-    }
-    entries.push({
-        title: req.body.title,
-        content: req.body.body, 
-        published: new Date()
-    })
-    res.redirect("/");
-})
+// Use expresses' static instead
+let staticFiles = path.join(__dirname, "static");
+app.use(express.static(staticFiles));
 
 // Response 404 if unknown source 
 app.use((req, res) => {
-    res.status(404).render("404");
+    res.status(404);
+    res.send("File not found!");
 })
 
 
