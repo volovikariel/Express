631b124cf9e853eb05bff541683ba80625224f9d
volovikariel <volovikariel@gmail.com>
2020-10-02 20:53:14 -0400

    Basic routing

--- script.js
+++ script.js
@@ -9,15 +9,14 @@ let app = express();
 // Use logger 
 app.use(morgan("short"));
 
-// Use expresses' static instead
-let staticFiles = path.join(__dirname, "static");
-app.use(express.static(staticFiles));
+app.get("/ariel", (req, res) => {
+    res.send("Hello ariel :D");
+})
 
 // Response 404 if unknown source 
 app.use((req, res) => {
     res.status(404);
     res.send("File not found!");
 })
-
 
 http.createServer(app).listen(3000);
